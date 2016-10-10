42698eb3d12955965b7224c5ada5924d4d2010be
TheThomasLeahy <thomasleahy.apps@gmail.com>
2016-10-09 22:56:29 -0500

    colorstuff
    kristen

--- CalcColorVariation.m
+++ CalcColorVariation.m
@@ -1,77 +1,32 @@
-function [ output_args ] = CalcColorVariation( BorderXY, ImageBorder)
-%Code by Laura Kenyon
+function [ColorVar] = CalcColorVariation(Image, BorderXY, ImageBorder)
+%Code by Laura Kenyon and Kristen Hagan 
 
 clc;
- %images2 = uigetdir('C:\', 'Select a folder of images you want to process!');
-% load images2
-% figure 
-% image(X)
-% colormap(map)
 
-colormap copper
+%output simple colormap 
 
-subplot(3,2, 1)
-A = imread('image1.gif');
-imagesc(A)
+imagesc(Image)
+colorbar
+title('ColorMap of Image');
+figure;
+%find homogeneity of only mole 
 
-% subplot(2,3, 2)
-% B = imread('image2.gif');
-% imagesc(B)
-% 
-% subplot(2,3,3)
-% C = imread('image3.gif');
-% imagesc(C)
-% 
-% subplot(2,3, 4)
-% D = imread('image4.gif');
-% imagesc(D)
+%mask Image with border 
+maskedImage = Image;
+bordermask = poly2mask(BorderXY(:,1), BorderXY(:,2), 512, 512);
+%imshow(bordermask);
+% filledmask= imfill(bordermask);
+% imshow(filledmask);
+%figure;
+title('Mask Used in ColorHomog. Calculation');
+maskedImage(~bordermask)=255;
+imshow(maskedImage);
+figure;
 
-subplot(3,2, 2)
-E =imread('colored_lesion1.gif');
-imagesc(E)
-
-subplot(3,2,3)
-imshowpair(A,E, 'falsecolor')
-
-subplot(3,2,4)
-imshowpair(A,E, 'blend')
-
-subplot(3,2,5)
-imshowpair(A,E, 'diff')
-
-subplot(3,2,6)
-imshowpair(A,E, 'montage')
-
-ssim(A,E)
-
-ssim(A,A)
-% subplot(2,2,1)
-% F= imread('color_lesion2.gif');
-% imagesc(F)
-% 
-% subplot(2,2,2)
-% G= imread('color_lesion3.gif');
-% imagesc(G)
-% 
-% subplot(2,2,3)
-% imshowpair(F,G, 'diff')
-
-
-
-%imagesc(A)
-%colorbar
-
-%surf(peaks) %3D shaded surface plot
-%colormap default
-%surf(colormap)
-
-% figure
-% a=[5 5 5; 4 4 4; 5 5 5];
-% cmap=colormap;
-% 
-% ax2 = subplot(2,1,2);
-% contour(a)
-% colormap(ax2,pink)
+%find std dev of intensities across mole 
+ColorVar = stdfilt(maskedImage);
+imshow(ColorVar);
+figure;
 
 end
 
