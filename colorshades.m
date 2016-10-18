e0ea036706732f4a714e9dd612296bdbc9ba6905
kristenhagan <kristenhagan@att.net>
2016-10-18 17:11:22 -0500

    for kristens pleasure

--- colorshades.m
+++ colorshades.m
@@ -1,6 +1,5 @@
 [A,map] = imread('palette.gif');
-imagesc(A); colormap(map);
-mask= roipoly();        %mask the color I want to know 
-
-
-
+imtool(A,map)
+%black = 0.15, 0.15, 0.15 or less =warning 
+%pink range 
+%white range 0.9412 0.9412 0.9412 = light gray and above is white 
