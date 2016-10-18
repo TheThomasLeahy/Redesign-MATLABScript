11039dcf1b815957ff2e2a9b7dfd564b383c9074
kristenhagan <kristenhagan@att.net>
2016-10-18 17:27:11 -0500

    shades

--- colorshades.m
+++ colorshades.m
@@ -3,3 +3,9 @@ imtool(A,map)
 %black = 0.15, 0.15, 0.15 or less =warning 
 %pink range 
 %white range 0.9412 0.9412 0.9412 = light gray and above is white 
+%tan 
+
+%provide number of colors 
+%std dev matrix for each 
+%images 
+%ssi over and over with the same mole across different 
