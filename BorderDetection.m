3aca2a4d4f069f042c50d83f0c8b352203b41039
mahykhussain <mahykhussain@gmail.com>
2016-10-17 20:14:04 -0500

    Border Detection with Pixel Conversion
    Makes the tick marks on a ruler and the lesion black and then find those
    borders (and puts them into an array). The cell array with the largest
    border is obviously the mole, so it returns that border. With the rest
    of the borders, it finds the centroids of the first two tick marks and
    uses the Pythagorean theorem to calculate the distance. This works in
    case the ruler is crooked or something. It also assumes that all the
    tick marks on a ruler are uniform.

--- BorderDetection.m
+++ BorderDetection.m
@@ -1,13 +1,27 @@
-function [ bound,I_bin_filled] = BorderDetection( I )
+function [bound,I_bin_filled,pixel_conversion] = BorderDetection( I )
 %Code by Mahy Hussain
 %Finds the border of a lesion image
-%Returns image and xy coordinates
+%Returns binarized image, xy coordinates, and pixel conversion (pixels/mm)
 
 I_bin = imbinarize(I);
 I_bin =imcomplement(I_bin);
 I_bin_filled = imfill(I_bin,'holes');
 B = bwboundaries(I_bin_filled);
 bound = B(1);
+moleIndex = 1;
+dim = size(cell2mat(bound));
+for i=2:length(B)
+    test = size(cell2mat(B(i)));
+    if test(1) > dim(1)
+        bound = B(i);
+        moleIndex = i;
+    end
+end
+c = regionprops(I_bin_filled,'centroid');
+centroids = cat(1, c.Centroid);
+centroids(moleIndex,:) = [];
+distances = diff(centroids);
+pixel_conversion = sqrt(distances(1,1)^2 + distances(1,2)^2);
 
 end
 
