48b445af5497719e09e112628ea1ad370e698a3c
mahykhussain <mahykhussain@gmail.com>
2016-10-19 14:50:51 -0500

    Better variables

--- BorderDetection.m
+++ BorderDetection.m
@@ -1,13 +1,14 @@
-function [bound,I_bin_filled] = BorderDetection( I )
+function [bound,I_filled] = BorderDetection( I )
 %Code by Mahy Hussain
 %Finds the border of a lesion image
 %Returns binarized image and xy coordinates
 
 I_bin = imbinarize(I);
-I_bin = imcomplement(I_bin);
-I_bin_filled = bwareaopen(I_filled,50);
-I_bin_filled = imfill(I_bin,'holes');
-B = bwboundaries(I_bin_filled);
+I_comp = imcomplement(I_bin);
+I_despeckle = bwareaopen(I_comp,50);
+I_filled = imfill(I_despeckle,'holes');
+B = bwboundaries(I_filled);
+bound = B(1);
 
 
 
