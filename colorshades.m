1cf2435c25caa4ec4e6d675894594f9ff31d2682
kristenhagan <kristenhagan@att.net>
2016-10-19 14:28:57 -0500

    hi

--- colorshades.m
+++ colorshades.m
@@ -1,5 +1,8 @@
-[A,map] = imread('palette.gif');
-imtool(A,map)
+[A,map] = imread('EditMole1.gif');
+[ruler, rulermap]=imcrop(A, map);
+figure;
+imshow(ruler); colormap(rulermap);
+
 %black = 0.15, 0.15, 0.15 or less =warning 
 %pink range 
 %white range 0.9412 0.9412 0.9412 = light gray and above is white 
@@ -9,3 +12,5 @@ imtool(A,map)
 %std dev matrix for each 
 %images 
 %ssi over and over with the same mole across different 
+
+
